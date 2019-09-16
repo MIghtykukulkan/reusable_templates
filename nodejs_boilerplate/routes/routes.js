356f996e39684bf1ed2814d0f44376b8318b9d2a
umashankar-rapiqube <uma.s.shankar@gmail.com>
2019-09-16 11:51:19 +0530

    adding few updates

--- nodejs_boilerplate/routes/routes.js
+++ nodejs_boilerplate/routes/routes.js
@@ -4,49 +4,9 @@ const mybusiness = require('../src/core/mybusiness');
 const to = require('await-to-js').default;
 var fs = require('fs');
 var db = require('../src/adapters/mongodb');
-var jwt = require('jsonwebtoken')
 const passport = require('passport');
 require('../src/services/passport')
-/*
-//---------------------------------
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
-const JWTStrategy   = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
 
-
-//hardcoded users need to be removed while working with DB
-var users = [
-    {
-      id: 1,
-      name: 'jonathanmh',
-      password: '%2yx4'
-    },
-    {
-      id: 2,
-      name: 'test',
-      password: 'test'
-    }
-  ];
-
-  var jwtOptions = {}
-  jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
-  jwtOptions.secretOrKey = 'secret string';
-  
-  var strategy = new JWTStrategy(jwtOptions, function(jwt_payload, next) {
-    console.log('payload received', jwt_payload);
-    // usually this would be a database call:
-    var user = users[_.findIndex(users, {id: jwt_payload.id})];
-    if (user) {
-      next(null, user);
-    } else {
-      next(null, false);
-    }
-  });
-  
-passport.use(strategy);
-//------------------------------------------------
-*/
 
 module.exports = router => {
 
